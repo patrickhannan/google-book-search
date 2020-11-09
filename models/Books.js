15240800c8c2c8bb66410126afe6579b9e2dabe8
Patrick Hannan <patrick@hannan.com>
2020-11-09 02:31:24 -0500

    created book model

---
+++ models/Books.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BooksSchema = new Schema({
+    title: {
+      type: String,
+      trim: true,
+      required: "Title is required",
+    },
+    authors: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    link: {
+      type: String,
+    },
+  });
+  
+  const Book = mongoose.model("Books", BooksSchema);
+
+module.exports = Book;
