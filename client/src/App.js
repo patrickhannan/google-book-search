59a02b8982624c9b24ee76a8052ee8fe3e190051
Patrick Hannan <patrick@hannan.com>
2020-11-09 13:24:32 -0500

    built jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Saved from "./containers/Saved/Saved";
 import Search from "./containers/Search/Search";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
     <Router>
     <Navbar />
+    <Jumbotron />
       <Switch>
         <Route exact path="/search" component={Search} />
         <Route exact path="/saved" component={Saved} />
---
+++ client/src/components/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron jumbotron-fluid mt-4 main-image">
+      <div className="container text-center">
+        <h1 className="display-4">(React) Google Books Search</h1>
+        <p className="lead main-text">
+          Search for and save books of interest
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
