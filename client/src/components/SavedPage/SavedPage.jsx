e8ec561f120d79a9bfd016a4ec3e0e14299bc1ce
Patrick Hannan <patrick@hannan.com>
2020-11-10 01:54:51 -0500

    had to redo the page because I started mixing in code that didnt match up

--- client/src/components/SavedPage/SavedPage.jsx
+++ client/src/components/SavedPage/SavedPage.jsx
@@ -1,47 +1,33 @@
+import React from 'react';
 import axios from "axios";
-import React, { useEffect, useState } from "react";
 
-const SavedPage = () => {
-  const [savedBooks, setSavedBooks] = useState([]);
+const SavedPage = (props) => {
 
-  useEffect(() => {
-    axios
-      .get("/api/books")
-      .then((response) => {
-        setSavedBooks(response.data);
-      })
-      .catch((err) => {
-        if (err) throw err;
-      });
-  });
+    const deleteBook = (e) => {
+        const id = e.target.getAttribute("id");
+        axios.delete(`/api/books/${id}`).then((res) => {
+            console.log(res)
+            props.loadAllBooks()
+        })
+    }
 
-  const deleteBook = (event) => {
-    axios
-      .delete(`/api/books/${event.target.id}`)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        if (err) throw err;
-      });
-  };
+    const viewBook = () => {
+      let path = props.link;
+      window.open(path);
+    };
 
-  const viewBook = () => {
-    let path = book.link;
-    window.open(path);
-  };
-
-  return (
-    <div className="container text-center">
+    return (
+        <div className="container text-center">
         <li className="list-group-item">
           <h2>{props.title}</h2>
-          <h3>By: {props.authors}</h3>
+          <h3>Written By: {props.authors}</h3>
           <p>{props.description}</p>
           <img src={props.image} />
           <button type="submit"  id={props.id} className="btn btn-danger mr-2" onClick={deleteBook}>Delete Book</button>
           <button type="submit" className="btn btn-secondary" onClick={viewBook}>View Book</button>
         </li>
       </div>
-  );
+    );
 };
+
 export default SavedPage;
